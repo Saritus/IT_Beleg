515e9be13531168a489f18d1cecb1b152db6ea6a
Sebastian Mischke <SebastianMischke@gmx.de>
2016-06-12 20:51:17 +0200

    Einfügen des Post-Erstellens

--- functions.php
+++ functions.php
@@ -1,10 +1,45 @@
 <?php
 if (isset ( $_POST ['Post-Title'] ) && isset ( $_POST ['Post'] )) {
-	$var = $_POST ['Post-Title'];
-	echo $var;
-	$post = $_POST ['Post'];
-	echo $post;
+	if (file_exists ( 'xml/posts.xml' )) {
+		$doc = simplexml_load_file ( 'xml/posts.xml' );
+		
+		$post = $doc->addChild ( 'Post' );
+		$post->addChild ( 'Title', $_POST ['Post-Title'] );
+		$post->addChild ( 'Text', $_POST ['Post'] );
+		$post->addChild ( 'Date', time () );
+		
+		$doc->asXML ( "xml/posts.xml" );
+		
+		header ( 'Location: index.php' );
+	} else {
+		$doc = new DOMDocument ( '1.0', 'utf-8' );
+		$doc->formatOutput = true;
+		
+		$posts = $doc->createElement ( 'Posts' );
+		$post = $doc->createElement ( 'Post' );
+		$title = $doc->createElement ( 'Title' );
+		$text = $doc->createElement ( 'Text' );
+		$date = $doc->createElement ( 'Date' );
+		
+		$doc->appendChild ( $posts );
+		$posts->appendChild ( $post );
+		
+		$post->appendChild ( $title );
+		$newTitle = $doc->createTextNode ( $_POST ['Post-Title'] );
+		$newTitle = $title->appendChild ( $newTitle );
+		
+		$post->appendChild ( $text );
+		$newText = $doc->createTextNode ( $_POST ['Post'] );
+		$newText = $text->appendChild ( $newText );
+		
+		$post->appendChild ( $date );
+		$newDate = $doc->createTextNode ( time () );
+		$newDate = $date->appendChild ( $newDate );
+		
+		$doc->preserveWhiteSpace = false;
+		$doc->save ( "./xml/posts.xml" );
+		
+		header ( 'Location: index.php' );
+	}
 }
-
-echo $_GET ['variable'];
 ?>
--- newPost.php
+++ newPost.php
@@ -91,17 +91,73 @@
 				</script>
 			</div>
 			<div class="col-lg-4">
-				<h2>MarkDown Editor</h2>
-				<p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus,
-					tellus ac cursus commodo, tortor mauris condimentum nibh, ut
-					fermentum massa justo sit amet risus. Etiam porta sem malesuada
-					magna mollis euismod. Donec sed odio dui.</p>
-				<p>
-					<a class="btn btn-primary" href="#" role="button">View details
-						&raquo;</a>
-				</p>
-				<!-- Kalender -->
-				<div class="monthly" id="mycalendar"></div>
+				<div class="container-fluid">
+					<div class="row" style="max-width: 500px; margin: auto;">
+
+						<h1 style="text-align: center;">Markdown Guide</h1>
+
+						<h4>Emphasis</h4>
+						<pre>**<strong>bold</strong>**
+*<em>italics</em>*
+~~<strike>strikethrough</strike>~~</pre>
+
+						<h4>Headers</h4>
+						<pre># Big header
+## Medium header
+### Small header
+#### Tiny header</pre>
+
+						<h4>Lists</h4>
+						<pre>* Generic list item
+* Generic list item
+* Generic list item
+
+1. Numbered list item
+2. Numbered list item
+3. Numbered list item</pre>
+
+						<h4>Links</h4>
+						<pre>[Text to display](http://www.example.com)</pre>
+
+						<h4>Quotes</h4>
+						<pre>> This is a quote.
+> It can span multiple lines!</pre>
+
+						<h4>
+							Images &nbsp; <small>Need to upload an image? <a
+								href="http://imgur.com/" target="_blank">Imgur</a> has a great
+								interface.
+							</small>
+						</h4>
+						<pre>![](http://www.example.com/image.jpg)</pre>
+
+						<h4>Tables</h4>
+						<pre>| Column 1 | Column 2 | Column 3 |
+| -------- | -------- | -------- |
+| John     | Doe      | Male     |
+| Mary     | Smith    | Female   |
+
+<em>Or without aligning the columns...</em>
+
+| Column 1 | Column 2 | Column 3 |
+| -------- | -------- | -------- |
+| John | Doe | Male |
+| Mary | Smith | Female |
+</pre>
+
+						<h4>Displaying code</h4>
+						<pre>`var example = "hello!";`
+
+<em>Or spanning multiple lines...</em>
+
+```
+var example = "hello!";
+alert(example);
+```</pre>
+
+
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
